d9120cb32450c99cbb5d75e4064966708eb2c34e
Vladyslav Tsap <vldzpp@gmail.com>
2023-05-10 20:07:17 +0300

    add endpoint for sign-up

--- social_network/urls.py
+++ social_network/urls.py
@@ -4,4 +4,5 @@ from social_network import views
 
 urlpatterns = [
     path('hello', views.hello_world),
+    path('sign-up', views.sign_up),
 ]
--- social_network/views.py
+++ social_network/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 
+from social_network.serializers import UserSerializer
+
 
 @api_view(['GET'])
 def hello_world(request):
     return JsonResponse({'message': 'Hello, world!'})
+
+
+@api_view(['POST'])
+def sign_up(request):
+    serializer = UserSerializer(data=request.data)
+    if not serializer.is_valid():
+        return JsonResponse(serializer.errors, status=400)
+
+    user = serializer.save()
+    return JsonResponse({'username': user.username}, status=200)
